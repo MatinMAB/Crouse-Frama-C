272d7917ec147832fb9118f13f1651e9c644fd5a
Matin <matin@Matin-Ubuntu-24.04>
2026-07-31 14:16:10 +0330

    test(eva): add harness for s32_mul_u32_s32

---
+++ harnesses/P0/eva_main_s32_mul_u32_s32.c
@@ -0,0 +1,21 @@
+#include "__fc_builtin.h"
+#include "mul_Ccode.h"
+
+int eva_main(void)
+{
+    u32 val1;
+    s32 val2;
+    s32 result;
+
+    val1 =
+        Frama_C_unsigned_long_interval(0UL, 0xFFFFFFFFUL);
+
+    val2 =
+        Frama_C_long_interval(-2147483647L - 1L, 2147483647L);
+
+    result =
+        s32_mul_u32_s32(val1, val2);
+
+    (void) result;
+    return 0;
+}
