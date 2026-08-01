6418418fef25d859c77849ab1681fbfa8f0c9d5d
Matin <matin@Matin-Ubuntu-24.04>
2026-08-01 11:07:50 +0330

    test(eva): add harness for rotateLeft

---
+++ harnesses/P0/eva_main_rotateLeft.c
@@ -0,0 +1,24 @@
+#include "__fc_builtin.h"
+#include "rotatefunc.h"
+
+int eva_main(void)
+{
+    u32 num;
+    u32 rotation;
+    u32 result;
+
+    num =
+        Frama_C_unsigned_long_interval(
+            0UL,
+            0xFFFFFFFFUL);
+
+    rotation =
+        Frama_C_unsigned_long_interval(
+            0UL,
+            0xFFFFFFFFUL);
+
+    result = rotateLeft(num, rotation);
+
+    (void) result;
+    return 0;
+}
