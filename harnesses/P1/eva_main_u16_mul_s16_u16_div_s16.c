31153c15a791c1bd6dc657c566ea403746a9e728
Matin <matin@Matin-Ubuntu-24.04>
2026-08-03 16:05:42 +0330

    test(eva): add harness for u16_mul_s16_u16_div_s16

---
+++ harnesses/P1/eva_main_u16_mul_s16_u16_div_s16.c
@@ -0,0 +1,34 @@
+#include "__fc_builtin.h"
+#include "div_Ccode.h"
+
+int eva_main(void)
+{
+    s16 factor1;
+    u16 factor2;
+    s16 denominator;
+    u16 result;
+
+    factor1 =
+        Frama_C_short_interval(
+            -32768,
+            32767);
+
+    factor2 =
+        Frama_C_unsigned_short_interval(
+            0,
+            65535);
+
+    denominator =
+        Frama_C_short_interval(
+            -32768,
+            32767);
+
+    result =
+        u16_mul_s16_u16_div_s16(
+            factor1,
+            factor2,
+            denominator);
+
+    (void) result;
+    return 0;
+}
